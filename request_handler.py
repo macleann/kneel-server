8d25a68c915cccb14f7caabf6248209c2c1d0a2a
Neil MacLean <nbmac13@gmail.com>
2023-04-10 16:08:04 -0500

    building out the fetch methods

---
+++ request_handler.py
@@ -0,0 +1,130 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from views import get_all_metals, get_single_metal, create_metal, delete_metal, get_all_styles, get_single_style, create_style, delete_style, get_all_sizes, get_single_size, create_size, delete_size, get_all_orders, get_single_order, create_order, delete_order
+
+
+class HandleRequests(BaseHTTPRequestHandler):
+    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
+    """
+
+    def do_GET(self):
+        """Handles GET requests to the server """
+        self._set_headers(200)
+        response = {}
+        
+        (resource, id) = self.parse_url(self.path)
+
+        if resource == "metals":
+            if id is not None:
+                response = get_single_metal(id)
+            else:
+                response = get_all_metals()
+                
+        elif resource == "styles":
+            if id is not None:
+                response = get_single_style(id)
+            else:
+                response = get_all_styles()
+        
+        elif resource == "sizes":
+            if id is not None:
+                response = get_single_size(id)
+            else:
+                response = get_all_sizes()
+                
+        elif resource == "orders":
+            if id is not None:
+                response = get_single_order(id)
+            else:
+                response = get_all_orders()
+
+        else:
+            response = []
+
+        self.wfile.write(json.dumps(response).encode())
+
+    def do_POST(self):
+        """Handles POST requests to the server """
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+        (resource, id) = self.parse_url(self.path)
+        new_resource = None
+        
+        if resource == "metals":
+            new_resource = create_metal(post_body)
+        elif resource == "styles":
+            new_resource = create_style(post_body)
+        elif resource == "sizes":
+            new_resource = create_size(post_body)
+        elif resource == "orders":
+            new_resource = create_order(post_body)
+                
+        self.wfile.write(json.dumps(new_resource).encode())
+
+    def do_PUT(self):
+        """Handles PUT requests to the server """
+        self.do_POST()
+        
+    def do_DELETE(self):
+        self._set_headers(204)
+        (resource, id) = self.parse_url(self.path)
+        
+        if resource == "metals":
+            delete_metal(id)
+        elif resource == "styles":
+            delete_style(id)
+        elif resource == "sizes":
+            delete_size(id)
+        elif resource == "orders":
+            delete_order(id)
+            
+        self.wfile.write("".encode())
+
+    def _set_headers(self, status):
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status (number): the status code to return to the front end
+        """
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_OPTIONS(self):
+        """Sets the options headers
+        """
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        try:
+            id = int(path_params[2])
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+        return (resource, id)
+
+# point of this application.
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
---
+++ views/order_requests.py
@@ -0,0 +1,61 @@
+ORDERS = [
+    {
+        "id": 1,
+        "metalId": 1,
+        "sizeId": 2,
+        "styleId": 3,
+        "timestamp": 1614659931693
+    },
+    {
+        "id": 2,
+        "metalId": 2,
+        "sizeId": 3,
+        "styleId": 1,
+        "timestamp": 1616333988188
+    },
+    {
+        "id": 3,
+        "metalId": 3,
+        "sizeId": 1,
+        "styleId": 2,
+        "timestamp": 1616334884289
+    },
+    {
+        "id": 4,
+        "metalId": 1,
+        "sizeId": 3,
+        "styleId": 2,
+        "timestamp": 1616334980694
+    }
+]
+
+def get_all_orders():
+    return ORDERS
+
+def get_single_order(id):
+    requested_order = None
+    
+    for order in ORDERS:
+        if order["id"] == id:
+            requested_order = order
+            return requested_order
+        
+def create_order(order):
+    if len(ORDERS) == 0:
+        max_id = 0
+    else:
+        max_id = ORDERS[-1]["id"]
+    new_id = max_id + 1
+    order["id"] = new_id
+    ORDERS.append(order)
+    return order
+
+def delete_order(id):
+    order_index = -1
+    
+    for index, order in enumerate(ORDERS):
+        if order["id"] == id:
+            order_index = index
+            
+    if order_index >= 0:
+        ORDERS.pop(order_index)
